cf828a41adf0d91c90368faf738da3afc4753278
Matfilius <87062144+Matfilius@users.noreply.github.com>
2025-07-19 18:40:52 +0200

    Revert "Merge branch 'main' of https://github.com/Matfilius/game-jam-matfi"
    This reverts commit f07b8ccbaa8fd7bb348b9338cc11dfb1ac55052c, reversing
    changes made to ff9151849ceee945468d5132a439ab574eb0d999.

--- Kenney Game Jam/Assets/Scripts/DoorScript.cs
+++ Kenney Game Jam/Assets/Scripts/DoorScript.cs
@@ -1,53 +1,33 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class DoorScript : MonoBehaviour
 {
     public dodir_sa_igracem touch;
-    public GameObject door;
+    public GameObject Player;
     public GameObject doorSet;
+    void Start()
+    {
+        
+    }
 
-    private bool canTeleport = true;
-    private float teleportCooldown = 0.5f; // Half a second
-    private float lastTeleportTime = -999f;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!canTeleport || Time.time - lastTeleportTime < teleportCooldown)
-            return;
-
-        // Player touches entrance door
-        if (collision.gameObject == door)
+        if(collision.tag == "KeyDoor")
         {
             if (touch.pickupKey)
             {
-                transform.position = doorSet.transform.position;
-                canTeleport = false;
-                lastTeleportTime = Time.time;
-                Debug.Log("Teleported to doorSet");
+                
             }
             else
             {
-                Debug.Log("Door is locked");
+
             }
-        }
-
-        // Player touches destination door
-        else if (collision.gameObject == doorSet)
-        {
-            Vector3 offset = new Vector3(0, -1f, 0);
-            transform.position = door.transform.position + offset;
-            canTeleport = false;
-            lastTeleportTime = Time.time;
-            Debug.Log("Teleported back to door");
-        }
-    }
-
-    private void Update()
-    {
-        // Re-enable teleport after cooldown
-        if (!canTeleport && Time.time - lastTeleportTime >= teleportCooldown)
-        {
-            canTeleport = true;
         }
     }
 }

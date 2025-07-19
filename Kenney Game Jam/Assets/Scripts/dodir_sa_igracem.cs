07c3e84e163b3419369be462c365093e38f37ddb
Matfilius <87062144+Matfilius@users.noreply.github.com>
2025-07-19 18:47:31 +0200

    aj sad

--- Kenney Game Jam/Assets/Scripts/dodir_sa_igracem.cs
+++ Kenney Game Jam/Assets/Scripts/dodir_sa_igracem.cs
@@ -5,12 +5,14 @@ public class dodir_sa_igracem : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     public bool pickupKey = false;
+    public GameObject key;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
-            Destroy(gameObject);
-            pickupKey = true;        }
+            pickupKey = true;
+            key.SetActive(false);        
+        }
     }
 
 

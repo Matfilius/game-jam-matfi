aaa71949558ccd3dae7681503e65e81cfc5d9125
Matfilius <87062144+Matfilius@users.noreply.github.com>
2025-07-19 18:55:30 +0200

    nike ovo da ne dira

--- Kenney Game Jam/Assets/Scripts/DoorScript.cs
+++ Kenney Game Jam/Assets/Scripts/DoorScript.cs
@@ -3,31 +3,51 @@ using UnityEngine;
 public class DoorScript : MonoBehaviour
 {
     public dodir_sa_igracem touch;
-    public GameObject Player;
+    public GameObject door;
     public GameObject doorSet;
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private bool canTeleport = true;
+    private float teleportCooldown = 0.5f; // Half a second
+    private float lastTeleportTime = -999f;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "KeyDoor")
+        if (!canTeleport || Time.time - lastTeleportTime < teleportCooldown)
+            return;
+
+        // Player touches entrance door
+        if (collision.gameObject == door)
         {
             if (touch.pickupKey)
             {
-                
+                transform.position = doorSet.transform.position;
+                canTeleport = false;
+                lastTeleportTime = Time.time;
+                Debug.Log("Teleported to doorSet");
             }
             else
             {
-
+                Debug.Log("Door is locked");
             }
+        }
+
+        // Player touches destination door
+        else if (collision.gameObject == doorSet)
+        {
+            Vector3 offset = new Vector3(0, -1f, 0);
+            transform.position = door.transform.position + offset;
+            canTeleport = false;
+            lastTeleportTime = Time.time;
+            Debug.Log("Teleported back to door");
+        }
+    }
+
+    private void Update()
+    {
+        // Re-enable teleport after cooldown
+        if (!canTeleport && Time.time - lastTeleportTime >= teleportCooldown)
+        {
+            canTeleport = true;
         }
     }
 }

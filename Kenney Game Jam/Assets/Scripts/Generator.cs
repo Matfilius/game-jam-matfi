651d7d72c6479fe2a9f0bf5f1bca8fbb75ddded2
Matfilius <87062144+Matfilius@users.noreply.github.com>
2025-07-20 08:24:22 +0200

    generator radi

--- Kenney Game Jam/Assets/Scripts/Generator.cs
+++ Kenney Game Jam/Assets/Scripts/Generator.cs
@@ -5,7 +5,11 @@ using System.Collections;
 public class Generator : MonoBehaviour
 {
     public int powerCellCount = 0;
+    public GameObject cell1;
+    public GameObject cell2;
+    public GameObject cell3;
     public CoroutineManager manager;
+    public bool levelPassed;
 
     public void AddPowerCell()
     {
@@ -13,5 +17,27 @@ public class Generator : MonoBehaviour
         manager.ShowPickupCellMessage();
     }
 
-    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if(powerCellCount == 1)
+            {
+                cell1.SetActive(true);
+            }
+            else if (powerCellCount == 2)
+            {
+                cell1.SetActive(true);
+                cell2.SetActive(true);
+            }
+            else if (powerCellCount == 3)
+            {
+                cell1.SetActive(true);
+                cell2.SetActive(true);
+                cell3.SetActive(true);
+                levelPassed = true;
+            }
+        }
+    }
+
 }
